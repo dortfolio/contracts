9bc9c5419acd056b7ddd47b3826bda20b0501de4
0xc2a5 <155046752+0xc2a5@users.noreply.github.com>
2024-06-01 09:33:10 -0700

    Update PortfolioToken.sol

--- v0/src/PortfolioToken.sol
+++ v0/src/PortfolioToken.sol
@@ -6,4 +6,12 @@ import {Owned} from "solmate/auth/Owned.sol";
 
 contract PortfolioToken is ERC20, Owned {
     constructor(address _owner, string memory _name, string memory _symbol) ERC20(_name, _symbol, 18) Owned(_owner) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
 }
